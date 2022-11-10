10b6623210b688c496c2b53daaf510519082ab50
cannn <ngoccanscitech@gmail.com>
2022-11-10 11:05:32 +0700

    update banner and element

--- js/myscript.js
+++ js/myscript.js
@@ -13,4 +13,35 @@ $(document).ready(function(){
       prevArrow:`<button type='button' class='slick-prev slick-arrow'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
         nextArrow:`<button type='button' class='slick-next slick-arrow'><ion-icon name="arrow-forward-outline"></ion-icon></button>`
     });
+
+    // Brands List
+    $('.brands-center-slider').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      centerMode: true,
+      arrows: true,
+      dots: false,
+      speed: 300,
+      infinite: true,
+      autoplaySpeed: 5000,
+      autoplay: false,
+      prevArrow:`<button type='button' class='slick-prev slick-arrow'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
+
+      responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+      });
   });
